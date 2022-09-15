84cf1f5f9c2fb7bb943f802d98078d58bf413273
BraianGalarza <galarzabraian01@gmail.com>
2022-09-14 23:00:25 -0300

    Primer entrega del proyecto final

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-const ItemDetails=({itemDetail})=> {
-    console.log(itemDetail)
+
+const ItemDetails=({item})=> {
+    console.log(item)
   return (
     <div className="container">
         <main className="container">
@@ -10,20 +11,20 @@ const ItemDetails=({itemDetail})=> {
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"/>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
                 <div className="tab-pane active" id="project1" role="tabpanel" aria-labelledby="project1-tab">
-                    <h1 className="my-4">{itemDetail.name}</h1>
+                    <h1 className="my-4">{item.name}</h1>
                     <div className="row">
                         <div className="col-md-8">
-                            <img className="img-fluid" src={itemDetail.image} alt=""/>
+                            <img className="img-fluid" src={item.image} alt=""/>
                         </div>
                         <div className="col-md-4">
                             <h3 className="my-3">Descripción</h3>
-                            {itemDetail.descripcion}
+                            {item.descripcion}
                             <h3 className="my-3">Detalles</h3>
                             <ul>
-                                <li>Stock: {itemDetail.stock}</li>
-                                <li>Precio c/u: {itemDetail.precio}</li>
-                                <li>Estado: {itemDetail.estado}</li>
-                                <li>Medios de Pago: {itemDetail.pago}</li>
+                                <li>Stock: {item.stock}</li>
+                                <li>Precio c/u: {item.precio}</li>
+                                <li>Estado: {item.estado}</li>
+                                <li>Medios de Pago: {item.pago}</li>
                             </ul>
                         </div>
                     </div>
